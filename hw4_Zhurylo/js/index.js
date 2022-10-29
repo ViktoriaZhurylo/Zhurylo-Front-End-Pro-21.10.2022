cf934d78e93d0ee49cde01811474bf6b4aca8f53
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-10-29 19:06:05 +0300

    hw4

---
+++ hw4_Zhurylo/js/index.js
@@ -0,0 +1,14 @@
+const firstNumber = +prompt('write first number')
+const secondNumber = +prompt('write second number')
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert('Write numbers!!!!!!')
+} else {
+    alert(`
+     Користувач ввів ${firstNumber} і ${secondNumber}:
+      ${firstNumber} + ${secondNumber} = ${firstNumber+secondNumber}
+      ${firstNumber} - ${secondNumber} = ${firstNumber-secondNumber}
+      ${firstNumber} * ${secondNumber} = ${firstNumber*secondNumber}
+      ${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}
+    `)
+}
+
