89b662f7929aa4b89075617c120f7059812b3f88
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-10-30 00:31:50 +0300

    hw6

---
+++ hw6_Zhurylo/js/index.js
@@ -0,0 +1,8 @@
+const hoursAmount = +prompt('write amount of hours')
+if (isNaN(hoursAmount)) {
+    alert('write amount of hours!!!')
+    location. reload()
+} else {
+    const secAmount = hoursAmount * 60 * 60
+    alert(secAmount)
+}
