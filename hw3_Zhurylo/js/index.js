b4137a452e2c0a4647fefe29ed54972d53cbddd5
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-10-30 17:31:55 +0200

    changed let to const

--- hw3_Zhurylo/js/index.js
+++ hw3_Zhurylo/js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (){
-    let userName = prompt('What is your name?')
-    let userAge = prompt('How old are you?')
-    let userCity = prompt('Where are you from?')
+    const userName = prompt('What is your name?')
+    const userAge = prompt('How old are you?')
+    const userCity = prompt('Where are you from?')
     alert(userAge + userName + userCity)
 
     const number = prompt('Enter a five digit number')
