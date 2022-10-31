9013025479b7574c7637923f36514cd56f295033
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-10-31 20:34:59 +0200

    add defer

--- hw6_Zhurylo/js/index.js
+++ hw6_Zhurylo/js/index.js
@@ -1,8 +1,12 @@
-const hoursAmount = +prompt('write amount of hours')
-if (isNaN(hoursAmount)) {
-    alert('write amount of hours!!!')
-    location. reload()
-} else {
-    const secAmount = hoursAmount * 60 * 60
-    alert(secAmount)
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const hoursAmount = +prompt('write amount of hours')
+    if (isNaN(hoursAmount)) {
+        alert('write amount of hours!!!')
+        location. reload()
+    } else {
+        const secAmount = hoursAmount * 60 * 60
+        alert(secAmount)
+    }
+
+})
+
