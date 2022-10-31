7a1cae23f6c7f998778804231f329303ca64b6fc
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-10-31 20:30:26 +0200

    add defer

--- hw5_Zhurylo/js/index.js
+++ hw5_Zhurylo/js/index.js
@@ -1,33 +1,39 @@
-const operation = prompt('write which operation: add, sub, mult, div?');
-if (operation !== 'add' && operation !== 'sub' && operation !== 'mult' && operation !== 'div') {
-    alert('Write an operation!!!!!!')
-} else {
-    const firstNumber = +prompt('write first number')
-    const secondNumber = +prompt('write second number')
-
-    if (isNaN(firstNumber) || isNaN(secondNumber)) {
-        alert('Write numbers!!!!!!')
+document.addEventListener('DOMContentLoaded', function (){
+    const operation = prompt('write which operation: add, sub, mult, div?');
+    if (operation !== 'add' && operation !== 'sub' && operation !== 'mult' && operation !== 'div') {
+        alert('Write an operation!!!!!!')
     } else {
-        if (operation === "add") {
-            alert(
-                `${firstNumber} + ${secondNumber} = ${firstNumber+secondNumber}`
-            )
-        }
-        if (operation === "sub") {
-            alert(
-                `${firstNumber} - ${secondNumber} = ${firstNumber-secondNumber}`
-            )
-        }
-        if (operation === "mult") {
-            alert(
-                `${firstNumber} * ${secondNumber} = ${firstNumber*secondNumber}`
-            )
-        }
-        if (operation === "div") {
-            alert(
-                `${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}`
-            )
+        const firstNumber = +prompt('write first number')
+        const secondNumber = +prompt('write second number')
+
+        if (isNaN(firstNumber) || isNaN(secondNumber)) {
+            alert('Write numbers!!!!!!')
+        } else {
+            if (operation === "add") {
+                alert(
+                    `${firstNumber} + ${secondNumber} = ${firstNumber+secondNumber}`
+                )
+            }
+            if (operation === "sub") {
+                alert(
+                    `${firstNumber} - ${secondNumber} = ${firstNumber-secondNumber}`
+                )
+            }
+            if (operation === "mult") {
+                alert(
+                    `${firstNumber} * ${secondNumber} = ${firstNumber*secondNumber}`
+                )
+            }
+            if (operation === "div") {
+                alert(
+                    `${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}`
+                )
+            }
         }
     }
-}
-location.reload()
+    location.reload()
+
+})
+
+
+
