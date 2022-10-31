6d7d8bc74f26e547c4d63cb2b3040dd312a42932
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-10-31 20:27:36 +0200

    add defer

--- hw4_Zhurylo/js/index.js
+++ hw4_Zhurylo/js/index.js
@@ -1,14 +1,18 @@
-const firstNumber = +prompt('write first number')
-const secondNumber = +prompt('write second number')
-if (isNaN(firstNumber) || isNaN(secondNumber)) {
-    alert('Write numbers!!!!!!')
-} else {
-    alert(`
+document.addEventListener('DOMContentLoaded', function (){
+    const firstNumber = +prompt('write first number')
+    const secondNumber = +prompt('write second number')
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        alert('Write numbers!!!!!!')
+    } else {
+        alert(`
      Користувач ввів ${firstNumber} і ${secondNumber}:
       ${firstNumber} + ${secondNumber} = ${firstNumber+secondNumber}
       ${firstNumber} - ${secondNumber} = ${firstNumber-secondNumber}
       ${firstNumber} * ${secondNumber} = ${firstNumber*secondNumber}
       ${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}
     `)
-}
+    }
+})
+
+
 
