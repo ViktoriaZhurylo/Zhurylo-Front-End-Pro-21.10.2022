3fbd411f074eea3ad8642a36bec4e1b5d5d8c70d
Viktoriia Zhurylo <viktoria.zhurylo@gmail.com>
2022-11-24 17:16:29 +0200

    hw14

---
+++ hw14_Zhurylo/js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const array = [1, 2, 3, 4, 5, 6, 7];
+    const userElement = Number(prompt(`Enter the item you want you remove from this list: ${array.join(', ')}`))
+    function removeElement (array, item) {
+        if (array.includes(item)) {
+            array.splice(item - 1, 1)
+            alert(`Now the list looks like this: ${array.join(', ')}`)
+
+        } else {
+            alert('The item you entered is not in the list. Please enter an item from the list')
+            location. reload()
+        }
+    }
+    removeElement(array, userElement );
+})
